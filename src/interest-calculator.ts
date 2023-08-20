ffd8d3c72859c238fe0e95ab744d58e6cb0f7827
maria-gallego <mariapaulagallegoc@gmail.com>
2023-08-20 15:47:19 +1000

    Adds validations to provided values

--- src/interest-calculator.ts
+++ src/interest-calculator.ts
@@ -1,3 +1,5 @@
+import {ValidationError} from "./error-handling";
+
 enum PaymentFrequency {
    MONTHLY = 12,
    QUARTERLY = 4,
@@ -14,21 +16,44 @@ export const paymentFrequencyValue = (paymentString: string): PaymentFrequency |
    if (getFrequencyEnum(paymentString) !== undefined) {
       return getFrequencyEnum(paymentString)
    } else {
-      return undefined
+      throw new ValidationError("Payment Frequency mst be Monthly, Quarterly, Annually or At Maturity")
    }
 }
 
+const validatedInterest = (annualInterest: number) => {
+   if (annualInterest > 0) {
+      return annualInterest/100
+   } else {
+      throw new ValidationError("Interest must be entered as a percentage greater than 0, like 1.5")
+   }
+}
+
+const validatedBalance = (balance: number) => {
+   if (balance > 1000) {
+      return balance
+   } else {
+   throw new ValidationError("The initial balance must be a number greater than 1000")
+   }
+}
+
+const validatedInvestmentPeriod = (investmentPeriod: number) => {
+   if (investmentPeriod > 1-12) {
+      return investmentPeriod
+   } else {
+      throw new ValidationError("The initial balance must be a number greater than 1000")
+   }
+}
 export const finalBalanceWithPeriods = (
     initialBalance: number,
     annualInterest: number,
     periodsInYear: string,
     investmentTerm: number
 ) => {
-   const periodsInYearValue = paymentFrequencyValue(periodsInYear)
-   const interest = annualInterest/100
-   const interestPerPeriod = interest / periodsInYearValue
-   const periodsInvested = periodsInYearValue * investmentTerm
-   return initialBalance * (1 + interestPerPeriod) ** periodsInvested
+   const paymentFrequency = paymentFrequencyValue(periodsInYear)
+   const interestValue = validatedInterest(annualInterest)
+   const interestPerPeriod = interestValue / paymentFrequency
+   const periodsInvested = paymentFrequency * validatedInvestmentPeriod(investmentTerm)
+   return validatedBalance(initialBalance) * (1 + interestPerPeriod) ** periodsInvested
 }
 
 export const finalBalanceAtMaturity = (
@@ -36,8 +61,8 @@ export const finalBalanceAtMaturity = (
     annualInterest: number,
     investmentTerm: number
 ) => {
-   const interest =  annualInterest/100
-   return (initialBalance * interest * investmentTerm) + initialBalance
+   const interestValue =  validatedInterest(annualInterest)
+   return (initialBalance * interestValue * validatedInvestmentPeriod(investmentTerm)) + initialBalance
 }
 
 export const finalBalance = (
